fdc029d3f1bcbaac16ddb596ada69a17f9bf8b5a
3lejon <Rodny.engblom@gmail.com>
2026-04-28 10:23:01 +0300

    player has jump cooldown

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,7 +3,9 @@
 public class PlayerMovement : MonoBehaviour
 { 
     [SerializeField] private float speed;
+    [SerializeField] private float jumpCooldown = 0.5f;
     private Rigidbody2D body;
+    private float jumpTimer;
  
     private void Awake()
     {
@@ -14,7 +16,13 @@ public class PlayerMovement : MonoBehaviour
     {
         body.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.linearVelocity.y);
  
-        if (Input.GetKey(KeyCode.Space))
+        if (jumpTimer > 0f)
+            jumpTimer -= Time.deltaTime;
+ 
+        if (Input.GetKeyDown(KeyCode.Space) && jumpTimer <= 0f)
+        {
             body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
+            jumpTimer = jumpCooldown;
+        }
     }
 }
