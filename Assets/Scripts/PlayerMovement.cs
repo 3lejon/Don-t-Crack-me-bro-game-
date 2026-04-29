06cfbead06d5c654360d1b995b6977bb262860cc
3lejon <Rodny.engblom@gmail.com>
2026-04-29 10:02:48 +0300

    added jump and movement animations along with death animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,25 +4,59 @@ public class PlayerMovement : MonoBehaviour
 { 
     [SerializeField] private float speed;
     [SerializeField] private float jumpCooldown = 0.5f;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private KeyCode inputJump = KeyCode.Space;
+    [SerializeField] private KeyCode inputLeft = KeyCode.A;
+    [SerializeField] private KeyCode inputRight = KeyCode.D;
     private Rigidbody2D body;
     private float jumpTimer;
+
+    [Header("Sprites")]// These variables are related to the different sprite renderers for each direction of movement
+   
+    public AnimatedSpriteRenderer spriteRendererJump;
+    public AnimatedSpriteRenderer spriteRendererLeft;
+    public AnimatedSpriteRenderer spriteRendererRight;
+     public AnimatedSpriteRenderer spriteRendererDeath;
+    private AnimatedSpriteRenderer activeSpriteRenderer;
  
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
     }
  
+    private void SetDirection(Vector2 direction, AnimatedSpriteRenderer spriteRenderer)
+    {
+        if (activeSpriteRenderer != null)
+            activeSpriteRenderer.enabled = false;
+        activeSpriteRenderer = spriteRenderer;
+        if (activeSpriteRenderer != null)
+            activeSpriteRenderer.enabled = true;
+    }
+ 
     private void Update()
     {
-        body.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.linearVelocity.y);
- 
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        body.linearVelocity = new Vector2(horizontal * speed, vertical * speed);
         if (jumpTimer > 0f)
             jumpTimer -= Time.deltaTime;
- 
         if (Input.GetKeyDown(KeyCode.Space) && jumpTimer <= 0f)
         {
-            body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
+            body.linearVelocity = new Vector2(body.linearVelocity.x, jumpForce);
             jumpTimer = jumpCooldown;
+            SetDirection(Vector2.up, spriteRendererJump);
+        }
+        else if (Input.GetKey(inputLeft))
+        {
+            SetDirection(Vector2.left, spriteRendererLeft);
+        }
+        else if (Input.GetKey(inputRight))
+        {
+            SetDirection(Vector2.right, spriteRendererRight);
+        }
+        else
+        {
+            SetDirection(Vector2.zero, activeSpriteRenderer);
         }
     }
 }
