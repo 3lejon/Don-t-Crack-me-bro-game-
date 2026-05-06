da74a01625d888fd15baf246c00f8bc3ce12a9b8
3lejon <Rodny.engblom@gmail.com>
2026-05-06 13:17:56 +0300

    modified stage3 main mecanic

--- Assets/Scripts/TheWall.cs
+++ Assets/Scripts/TheWall.cs
@@ -6,27 +6,37 @@ public class TheWall : MonoBehaviour
     public float acceleration = 0.5f;
     private Vector2 velocity;
     private float currentSpeed;
+    private Collider2D wallCollider;
    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         currentSpeed = moveSpeed;
-        velocity = new Vector2(currentSpeed, 0); 
+        velocity = new Vector2(currentSpeed, 0);
+        wallCollider = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
-{
-    if (Time.time >= 20f && Time.time < 50f)
     {
-        currentSpeed += acceleration * Time.deltaTime;
-        velocity = new Vector2(currentSpeed, 0);
-    }
-    else if (Time.time >= 50f)
-    {
-        velocity = Vector2.zero;
-    }
+        if (Time.time >= 20f)
+        {
+            wallCollider.enabled = false;
+            if (Time.time < 50f)
+            {
+                currentSpeed += acceleration * Time.deltaTime;
+                velocity = new Vector2(currentSpeed, 0);
+            }
+            else
+            {
+                velocity = Vector2.zero;
+            }
+        }
+        else
+        {
+            wallCollider.enabled = true;
+        }
 
-    transform.Translate(velocity * Time.deltaTime);
-}
+        transform.Translate(velocity * Time.deltaTime);
+    }
 }
